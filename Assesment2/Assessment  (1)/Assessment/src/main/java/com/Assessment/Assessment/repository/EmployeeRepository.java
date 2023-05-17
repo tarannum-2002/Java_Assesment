7c562e7ad41fc172dae4e5f90ac7132df5978535
tarannum-2002 <tarannum.cs20@bmsce.ac.in>
2023-05-17 19:26:43 +0530

    fist solution

--- Assesment2/Assessment  (1)/Assessment/src/main/java/com/Assessment/Assessment/repository/EmployeeRepository.java
+++ Assesment2/Assessment  (1)/Assessment/src/main/java/com/Assessment/Assessment/repository/EmployeeRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-    @Query("SELECT e FROM Employee e WHERE e.d_no = :dno")
-    List<Employee> findByDno(int dno);
-
-    @Query("SELECT e FROM Employee e JOIN e.department d WHERE d.dname = :dname")
-    List<Employee> findByDname(String dname);
+//    @Query("SELECT e FROM Employee e WHERE e.d_no = :dno")
+//    List<Employee> findByDno(int dno);
+//
+//    @Query("SELECT e FROM Employee e JOIN e.department d WHERE d.dname = :dname")
+//    List<Employee> findByDname(String dname);
 }
 
 
